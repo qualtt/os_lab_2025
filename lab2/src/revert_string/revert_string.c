841bbf65bbd0dffb8bb4eff3a549da1605f7c184
qualtt <qualtt.io@gmail.com>
2025-09-27 11:19:09 +0000

    lab2 done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
-}
+    char *start = str;
+	char *end = start+strlen(str)-1;
 
+	while(start<end)
+	{
+		char temp = *end;
+		*end = *start;
+		*start=temp;
+		start++;
+		end--;
+	}
+}
